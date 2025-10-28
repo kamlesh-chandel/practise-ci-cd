88f34810dd2165967466c6ee499dd97e28f64f52
kamlesh-chandel <kamlesh@gkmit.co>
2025-10-28 18:59:08 +0530

    dsfsd

--- tests/test _operation.py
+++ tests/test _operation.py
@@ -2,8 +2,8 @@ from src.math_operations import add, sub
 
 def test_add():
     assert add(4,5) == 9
-    assert add(1,3) == 5
+    assert add(1,3) == 4
 
 def test_sub():
     assert sub(5,2) == 3
-    assert sub(5,3) == 1
+    assert sub(5,3) == 2
